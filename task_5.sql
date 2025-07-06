1b5882fc6af49cceb40b33da127c89231d8e4d1a
bryanmwangindungu <bryanmwangi52@gmail.com>
2025-07-06 23:43:42 +0300

    6th commit

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,4 @@
-CREATE TABLE IF NOT EXISTS customer (
-    customer_id INT PRIMARY KEY,
-    customer_name VARCHAR(215),
-    email VARCHAR(215),
-    address TEXT
-);
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
